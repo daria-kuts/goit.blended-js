b36dba2da53802f9afea926e35a933287270af54
Daria <tkachenko.darya95@gmail.com>
2025-08-19 21:05:10 +0300

    task-10

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -55,6 +55,7 @@
 //    default: result = "Вибачте, але ви маєте ввести значення від 1 до 4 включно"
 //}
 //console.log(result);
+
 //task4
 // Отримуйте від користувача число (кількість хвилин) через prompt
 // і виводьте у консоль рядок у форматі годин та хвилин.
@@ -83,7 +84,7 @@
 //    const pas = prompt("Введіть пароль");
 //    if (pas === "Я головний") {
 //        alert("Добрий день!");
-//    } else { 
+//    } else {
 //        alert("Невірний пароль!");
 //  }
 //} else if (!login) {
@@ -94,49 +95,59 @@
 
 //task6
 // Використайте цикл while, щоб вивести в console всі числа від 0 до 20 включно.
+function letNumbers(min, max) {
+     let count = min
+    while (count <= max) {
+        console.log(count);
+        count++
+    };
+}
+   
+letNumbers(0,20)
 
 //task7
-// Напишіть функцію getNumbers(min, max), що приймає 2 параметри - 
+// Напишіть функцію getNumbers(min, max), що приймає 2 параметри -
 // мінімальне і максимальне число відповідно.
 // Напишіть цикл всередині функції, який виводить у консоль
 // всі числа від max до min за спаданням.
 // Окрім цього, підрахуйте суму всіх парних чисел в цьому циклі
 // і поверніть її з функції.
-function getNumbers(min, max) {
-    if (typeof min !== 'number' || typeof max !== 'number') {
-        return 'Not a number';
-    }
-    let sum = 0;
-    for (let i = max; i >= min; i--) {
-        console.log(i);
-        if (i % 2 === 0) {
-            sum += i;
-        } 
-    }
-    return sum;
-}
-console.log(getNumbers(1, 12));
+// function getNumbers(min, max) {
+//     if (typeof min !== 'number' || typeof max !== 'number') {
+//         return 'Not a number';
+//     }
+//     let sum = 0;
+//     for (let i = max; i >= min; i--) {
+//         console.log(i);
+//         if (i % 2 === 0) {
+//             sum += i;
+//         }
+//     }
+//     return sum;
+// }
+// console.log(getNumbers(1, 12));
 
 //task8
 // Напишіть функцію min(a, b), яка приймає 2 числа і повертає меньше з них.
 // Потрібно додати перевірку, що функція отримує саме числа, в іншому випадку
 // повертати з функції рядок - 'Not a number!'.
-function min(a, b) {
-    if (typeof a !== 'number' || typeof b !== 'number') {
-        return 'Not a number!'
-    }
-    return Math.min(a, b);
-}
-console.log(min(10, 50));
+// function min(a, b) {
+//     if (typeof a !== 'number' || typeof b !== 'number') {
+//         return 'Not a number!'
+//     }
+//     return Math.min(a, b);
+// }
+// console.log(min(10, 50));
+
 //task 9
-// Напишіть функцію isAdult(age), яка  приймає число - вік користувача і повертає true, 
+// Напишіть функцію isAdult(age), яка  приймає число - вік користувача і повертає true,
 // якщо параметр age більше чи дорівнює 18.
-// В іншому випадку вона запитує підтвердження через confirm 
+// В іншому випадку вона запитує підтвердження через confirm
 // і повертає його результат (true/false).
-function isAdult(age) {
-    return age >= 18 || confirm("Вам є 18 років?");
-};
-console.log(isAdult(15));
+// function isAdult(age) {
+//     return age >= 18 || confirm("Вам є 18 років?");
+// };
+// console.log(isAdult(15));
 //    if (age >= 18) {
 //        return 'true';
 //    }
@@ -144,11 +155,28 @@ console.log(isAdult(15));
 //}
 //console.log(isAdult(15));
 
-function findNumber(start, end, divisor) {
-    for (let i = 0; i + start <= end; i++) {
-    if( (i + start) % divisor === 0 ) {
-      return i + start;
-      };
-    };
-}
-console.log(findNumber(2, 6, 5));
+// function findNumber(start, end, divisor) {
+//     for (let i = 0; i + start <= end; i++) {
+//     if( (i + start) % divisor === 0 ) {
+//       return i + start;
+//       };
+//     };
+// }
+// console.log(findNumber(2, 6, 5));
+//task10
+// Напишіть функцію fizzBuzz(num), яка приймає число і перевіряє кожне число від 1 до num:
+// Якщо число ділитися  без остачі на 3 - виводить в консоль 'fizz',
+// якщо ділиться  без остачі на 5 - виводить в консоль 'buzz',
+// якщо ділиться  без остачі і на 3, і на 5 - виводить в консоль 'fizzbuzz'.
+// function fizzBuzz(num) {
+//     for (i = 1; i <= num; i++){
+//         if (num % 3 === 0 && num % 5 === 0) {
+//             return 'fizzbuzz';
+//         } else if (num % 5 === 0) {
+//             return 'buzz';
+//         } else if (num % 3 === 0) {
+//             return 'fizz';
+//        }
+//     }
+// }
+// console.log(fizzBuzz(15))
