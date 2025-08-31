8eec460480ab98da37610ca5e9e9d7db55db9008
Daria <tkachenko.darya95@gmail.com>
2025-08-31 17:58:14 +0300

    les2

---
+++ js/lesson-2.js
@@ -0,0 +1,134 @@
+// task1
+// Створіть масив styles з елементами 'jazz' і 'blues'
+// Додайте до кінця масиву елемент 'rock-n-roll' за допомогою відповідного методу масивів
+// Знайдіть елемент 'blues' у масиві та замініть його на 'classic', використовуючи JavaScript-код
+
+// Напишіть функцію logItems(array), яка приймає масив як аргумент
+// і виводить у консоль кожен його елемент у форматі:
+// "<номер елемента> - <значення елемента>".
+// Використайте цикл for для перебору елементів масиву.
+// Нумерація елементів повинна починатися з 1 (а не з 0).
+const styles = ["jazz", "blues"]
+styles.push("rock-n-roll");
+const index = styles.indexOf("blues");
+if (index !== -1) {
+    styles[index] = "classic";
+};
+function logItems(array) {
+    for (let i = 0; i < array.length; i++){
+        console.log(`${i + 1} - ${array[i]}`);
+    }
+}
+logItems(styles);
+// task2
+// Напишіть функцію checkLogin(array), яка:
+// Приймає масив логінів як аргумент.
+// Запитує ім'я користувача через prompt.
+// Перевіряє, чи є введене ім'я у переданому масиві.
+// Якщо ім'я є в масиві – виводить повідомлення через alert: "Welcome, <name>!"
+// Якщо ім'я відсутнє – виводить повідомлення: "User not found".
+
+// const logins = ["Peter", "John", "Igor", "Sasha"];
+// function checkLogin(array) {
+//     const userName = prompt("Введіть ім'я");
+//     if (array.includes(userName)) {
+//         alert(`Welcome, ${userName}!`);
+//     } else {
+//         alert ("User not found");
+//     }
+// };
+// checkLogin(logins);
+
+// task3
+// Напишіть функцію caclculateAverage(),
+// яка приймає довільну кількість
+// аргументів і повертає їхнє середнє значення.
+// Додайте перевірку, що аргументи - це числа.
+
+function caclculateAverage() {
+    let total = 0;
+    let count = 0;
+    for (let argument of arguments) {
+        if (typeof argument === 'number') {
+            total += argument;
+            count++;
+        };
+    }
+     return total / count || 0;
+};
+console.log(caclculateAverage(9, 5, "7", 0, 3));
+console.log(caclculateAverage());
+
+// task4
+// Напишіть функцію, яка сумуватиме сусідні числа
+// і пушитиме їх в новий масив.
+
+// const someArr = [22, 11, 34, 5, 12, 13, 14, 15];
+
+// уточнення: складати необхідно перше число з другим, потім друге - з третім,
+// третє - з четвертим і так до кінця.
+// В результаті функція має повертати масив [33, 45, 39, 17, 25, 27, 29].
+
+function makeNewArr(array) {
+    const newArr = [];
+    for (let i = 0; i < array.length - 1; i++){
+        newArr.push(array[i] + array[i + 1]);
+    };
+    return newArr;
+};
+const someArr = [22, 11, 34, 5, 12, 13, 14, 15];
+console.log(makeNewArr(someArr));
+// task7
+// Напишіть скрипт, який для об'єкту user, послідовно:
+// 1 - додасть поле mood зі значенням 'happy',
+// 2 - замінить hobby на 'skydiving',
+// 3 - замінить значення premium на false,
+// 4 - виведе зміст об'єкта user у форматі
+// '<ключ>:<значення>' використовуя Object.keys() та for...of
+
+const user = {
+    name: "John",
+    age: 20,
+    hobby: "tenis",
+    premium: true,
+  };
+user.mood = 'happy';
+user.hobby = 'skydiving'
+user.premium = false;
+const userKeys = Object.keys(user);
+for (const key of userKeys) {
+    console.log(`${key}:${user[key]}`);
+};
+// task8
+// Є об'єкт, в якому зберігаються зарплати команди
+// Напишіть код для додавання усіх зарплат та
+// збережіть його результат в змінній sum.
+// Якщо об'єкт salaries пустий, то результат має бути 0
+
+const salaries = {
+    Mango: 100,
+    Poly: 160,
+    Ajax: 1470,
+};
+let sum = 0;
+const userSalaries = Object.values(salaries);
+for (const salarie of userSalaries) {
+
+    sum += salarie;
+};
+console.log(sum);
+// task9
+// Створіть об'єкт calculator з наступними методами:
+// read(a, b) - приймає два аргумента і зберігає їх як властивості об'єкта,
+// sum() - повертає сумму збереженних значень (з перевіркою на наявність властивостей в об'єкті),
+// mult() - перемножає збереженні значення і повертає результат (з перевіркою на наявність властивостей в об'єкті),
+// винесіть перевірку на наявність властивостей в об'єкті в окремий метод exist().
+
+// Якщо вказані властивості в обʼєкті відсутні (тобто метод exist повертає false),
+// методи sum і mult мають повертати рядок 'No such propeties'
+const calculator = {
+    read(a, b) {
+        this.a = a;
+        this.b = b;
+     }
+}
