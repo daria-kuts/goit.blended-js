49e875b8a7c1a756b87bfe5beea79b1c92c229de
Daria <tkachenko.darya95@gmail.com>
2025-09-14 17:02:16 +0300

    14.09

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -8,18 +8,18 @@
 // "<номер елемента> - <значення елемента>".
 // Використайте цикл for для перебору елементів масиву.
 // Нумерація елементів повинна починатися з 1 (а не з 0).
-const styles = ["jazz", "blues"]
-styles.push("rock-n-roll");
-const index = styles.indexOf("blues");
-if (index !== -1) {
-    styles[index] = "classic";
-};
-function logItems(array) {
-    for (let i = 0; i < array.length; i++){
-        console.log(`${i + 1} - ${array[i]}`);
-    }
-}
-logItems(styles);
+// const styles = ["jazz", "blues"]
+// styles.push("rock-n-roll");
+// const index = styles.indexOf("blues");
+// if (index !== -1) {
+//     styles[index] = "classic";
+// };
+// function logItems(array) {
+//     for (let i = 0; i < array.length; i++){
+//         console.log(`${i + 1} - ${array[i]}`);
+//     }
+// }
+// logItems(styles);
 // task2
 // Напишіть функцію checkLogin(array), яка:
 // Приймає масив логінів як аргумент.
@@ -45,19 +45,19 @@ logItems(styles);
 // аргументів і повертає їхнє середнє значення.
 // Додайте перевірку, що аргументи - це числа.
 
-function caclculateAverage() {
-    let total = 0;
-    let count = 0;
-    for (let argument of arguments) {
-        if (typeof argument === 'number') {
-            total += argument;
-            count++;
-        };
-    }
-     return total / count || 0;
-};
-console.log(caclculateAverage(9, 5, "7", 0, 3));
-console.log(caclculateAverage());
+// function caclculateAverage() {
+//     let total = 0;
+//     let count = 0;
+//     for (let argument of arguments) {
+//         if (typeof argument === 'number') {
+//             total += argument;
+//             count++;
+//         };
+//     }
+//      return total / count || 0;
+// };
+// console.log(caclculateAverage(9, 5, "7", 0, 3));
+// console.log(caclculateAverage());
 
 // task4
 // Напишіть функцію, яка сумуватиме сусідні числа
@@ -69,15 +69,31 @@ console.log(caclculateAverage());
 // третє - з четвертим і так до кінця.
 // В результаті функція має повертати масив [33, 45, 39, 17, 25, 27, 29].
 
-function makeNewArr(array) {
-    const newArr = [];
-    for (let i = 0; i < array.length - 1; i++){
-        newArr.push(array[i] + array[i + 1]);
-    };
-    return newArr;
-};
-const someArr = [22, 11, 34, 5, 12, 13, 14, 15];
-console.log(makeNewArr(someArr));
+// function makeNewArr(array) {
+//     const newArr = [];
+//     for (let i = 0; i < array.length - 1; i++){
+//         newArr.push(array[i] + array[i + 1]);
+//     };
+//     return newArr;
+// };
+// const someArr = [22, 11, 34, 5, 12, 13, 14, 15];
+// console.log(makeNewArr(someArr));
+
+// task5
+// Напишіть функцію findSmallestNumber(numbers),
+// яка шукає найменше число в масиві.
+// Додайте перевірку, що функція отримує саме масив, і
+// якщо функція отримує масив - поверніть з функції найменше число,
+// в іншому випадку - поверніть 'Sory, it is not an array!'.
+
+// const numbers = [2, 5, 35, 56, 12, 24, 7, 80, 3];
+// function findSmallestNumber(numbers) {
+//     if (Array.isArray(numbers) ) {
+//         return Math.min(numbers);
+//     }
+// }
+
+
 // task7
 // Напишіть скрипт, який для об'єкту user, послідовно:
 // 1 - додасть поле mood зі значенням 'happy',
@@ -86,37 +102,37 @@ console.log(makeNewArr(someArr));
 // 4 - виведе зміст об'єкта user у форматі
 // '<ключ>:<значення>' використовуя Object.keys() та for...of
 
-const user = {
-    name: "John",
-    age: 20,
-    hobby: "tenis",
-    premium: true,
-  };
-user.mood = 'happy';
-user.hobby = 'skydiving'
-user.premium = false;
-const userKeys = Object.keys(user);
-for (const key of userKeys) {
-    console.log(`${key}:${user[key]}`);
-};
+// const user = {
+//     name: "John",
+//     age: 20,
+//     hobby: "tenis",
+//     premium: true,
+//   };
+// user.mood = 'happy';
+// user.hobby = 'skydiving'
+// user.premium = false;
+// const userKeys = Object.keys(user);
+// for (const key of userKeys) {
+//     console.log(`${key}:${user[key]}`);
+// };
 // task8
 // Є об'єкт, в якому зберігаються зарплати команди
 // Напишіть код для додавання усіх зарплат та
 // збережіть його результат в змінній sum.
 // Якщо об'єкт salaries пустий, то результат має бути 0
 
-const salaries = {
-    Mango: 100,
-    Poly: 160,
-    Ajax: 1470,
-};
-let sum = 0;
-const userSalaries = Object.values(salaries);
-for (const salarie of userSalaries) {
+// const salaries = {
+//     Mango: 100,
+//     Poly: 160,
+//     Ajax: 1470,
+// };
+// let sum = 0;
+// const userSalaries = Object.values(salaries);
+// for (const salarie of userSalaries) {
 
-    sum += salarie;
-};
-console.log(sum);
+//     sum += salarie;
+// };
+// console.log(sum);
 // task9
 // Створіть об'єкт calculator з наступними методами:
 // read(a, b) - приймає два аргумента і зберігає їх як властивості об'єкта,
@@ -126,9 +142,9 @@ console.log(sum);
 
 // Якщо вказані властивості в обʼєкті відсутні (тобто метод exist повертає false),
 // методи sum і mult мають повертати рядок 'No such propeties'
-const calculator = {
-    read(a, b) {
-        this.a = a;
-        this.b = b;
-     }
-}
+// const calculator = {
+//     read(a, b) {
+//         this.a = a;
+//         this.b = b;
+//      }
+// }
---
+++ js/lesson-3.js
@@ -0,0 +1,303 @@
+// Дано масив чисел [1, 2, 3, 4, 5]. Створіть новий масив, що містить 
+// квадрати кожного елементу вхідного масиву. 
+// Очікуваний результат: [1, 4, 9, 16, 25].
+
+// const numbers = [1, 2, 3, 4, 5]
+// const newArr = numbers.map(number => number * number);
+// console.log(newArr);
+ 
+// task2
+// Дано масив об'єктів. Створіть новий масив, що містить всі значення 
+// з масивів values кожного об'єкту, збережених в одному масиві. 
+// Очікуваний результат: [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+const data = [
+ { id: 1, values: [1, 2, 3] },
+ { id: 2, values: [4, 5, 6] },
+ { id: 3, values: [7, 8, 9] },
+];
+const newData = data.flatMap((obj) =>obj.values) 
+console.log(newData);
+
+// task-3
+// Дано масив об'єктів. 
+// Перевірте, чи є хоча б один об'єкт з віком менше 20 років. 
+// Очікуваний результат: true.
+
+const people = [
+ { name: 'John', age: 27 },
+ { name: 'Jane', age: 31 },
+ { name: 'Bob', age: 19 },
+];
+const age = people.some(obj => obj.age < 20);
+console.log(age);
+
+
+// task-4
+// Дано масив чисел [2, 4, 6, 8, 10]. 
+// Перевірте, чи є кожен елемент масиву парним. Очікуваний результат: true.
+
+// const numbers = [2, 4, 6, 8, 10];
+// const newArr = numbers.every((number) => number % 2 === 0);
+// console.log(newArr);
+
+// task-5
+// Знайдіть перше непарне число
+
+// const numbers = [2, 1, 6, 8, 9, 10, 12];
+// const numberFind = numbers.find((number) => number % 2 !== 0)
+// console.log(numberFind);
+
+// task-6
+// Відсортуйте масив чисел [4, 2, 5, 1, 3] 
+// у порядку зростання. Очікуваний результат: [1, 2, 3, 4, 5].
+
+const numbersArray = [4, 200, 5, 10, 30];
+const newArray = numbersArray.toSorted((a, b) => a-b)
+console.log(newArray);
+// task-7
+// Відсортуйте масив рядків ["banana", "orange", "apple", "pear"]
+//  у порядку алфавіту. 
+// Очікуваний результат: ["apple", "banana", "orange", "pear"].
+
+const stringArray = ['banana', 'orange', 'apple', 'pear'];
+const stringSorted = stringArray.toSorted((a, b) => a.localeCompare(b));
+console.log(stringSorted);
+
+// task-8
+//  Відсортуйте масив об'єктів за віком у порядку зростання. 
+// Очікуваний результат: [{name: "Bob", age: 19}, {name: "John", age: 27}, {name: "Jane", age: 31}].
+
+const users = [
+ { name: 'John', age: 27 },
+ { name: 'Jane', age: 31 },
+ { name: 'Bob', age: 19 },
+];
+const usersSorted = users.toSorted((userA, userB) => userA.age - userB.age);
+console.log(usersSorted);
+
+// task-9
+// Дано масив об'єктів. 
+// Створіть новий масив, що містить тільки об'єкти, в яких 
+// вік більше 20 років. 
+// Очікуваний результат: [{name: "John", age: 27}, {name: "Jane", age: 31}]
+
+const user = [
+ { name: 'John', age: 27 },
+ { name: 'Jane', age: 31 },
+ { name: 'Bob', age: 19 },
+]
+const userFiltered = user.filter((obj) => obj.age > 20);
+console.log(userFiltered);
+
+// task-10
+// Дано масив чисел [1, 2, 3, 4, 5]. 
+// Застосуйте метод для обчислення суми елементів масиву.
+
+const numbers = [1, 2, 3, 4, 5];
+const total = numbers.reduce((previousValue, number) => {
+    return previousValue + number;
+}, 0)
+console.log(total);
+
+// маємо масив об'єктів, який представляє список товарів і їх цін:
+// Завдання: Знайдіть суму вартості всіх товарів, які коштують більше 10 гривень.
+const products = [
+  { name: 'Milk', price: 20 },
+  { name: 'Bread', price: 10 },
+  { name: 'Eggs', price: 15 },
+];
+const newProducts = products.filter((obj) => obj.price > 10).reduce((previousValue, num) => previousValue + num.price, 0);
+console.log(newProducts);
+
+
+// Напишіть функцію getGirlsPassports() яка поверне масив номерів паспортів дівчат.
+const friends = [
+    { passport: '03005988', name: 'Joseph Francis Tribbiani Jr', age: 32, sex: 'm' },
+    { passport: '03005989', name: 'Chandler Muriel Bing', age: 33, sex: 'm' },
+    { passport: '03005990', name: 'Ross Eustace Geller', age: 33, sex: 'm' },
+    { passport: '03005991', name: 'Rachel Karen Green', age: 31, sex: 'f' },
+    { passport: '03005992', name: 'Monica Geller', age: 31, sex: 'f' },
+    { passport: '03005993', name: 'Phoebe Buffay', age: 34, sex: 'f' }
+]
+const onlyGirls = friends.filter((obj) => obj.sex === 'f')
+    .map(friend => friend.passport);
+console.log(onlyGirls);
+
+// //Напиши клас Rectangle який створює об'єкт
+//з ​​властивостями height, width
+//і методом calculateArea() для підрахунку прлощі прямокутника.
+class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    calculateArea() {
+        return this.width * this.height;
+    }
+}
+
+// Завдання 12:
+ // Напиши клас Client який створює об'єкт з властивостями login email.
+ // Оголоси приватні властивості #login #email, доступ до яких зроби 
+ // через геттер та сеттер login email
+class Client {
+    #login;
+    #email;
+    constructor(login, email) {
+        this.#login = login;
+        this.#email = email;
+}
+    getLogin() {
+        return this.#login;
+    }
+    setLogin(newLogin) {
+        this.#login = newLogin;
+    }
+    getEmail() {
+        return this.#email;
+    }
+    setEmail(newEmail) {
+        this.#email = newEmail;
+    }
+};
+const client1 = new Client("user", "user@.mail")
+client1.login = "newlogin";
+client1.email = "newEmail@";
+client1.email = "newemail.mail";
+console.log(client1.email);
+
+ // Розроби клас Calculator, який дозволяє виконувати арифметичні 
+ //  операції над числом за допомогою методів класу, підтримуючи  
+ // ланцюжковий виклик (method chaining).
+ //
+ // Вимоги до класу Calculator
+ // - Метод number(value)
+ // Встановлює початкове значення для наступних обчислень.
+ // Повертає сам об'єкт (this) для підтримки ланцюжкових викликів.
+ 
+ // - метод getResult, Повертає поточний результат усіх операцій.
+ // Не змінює значення, просто повертає його.
+ 
+ // - метод add - Додає value до поточного значення.
+ // Повертає сам об'єкт (this) для підтримки ланцюжкових викликів.
+ 
+ // - метод substruct - Віднімає value від поточного значення. Повертає this.
+ 
+// - метод divide - Ділить поточне значення на value, якщо value не дорівнює 0.
+// Якщо value === 0, викидає помилку з повідомленням про неможливість ділення.
+// Повертає сам об'єкт (this) для підтримки ланцюжкових викликів.
+ 
+ // - метод multiply -Множить поточне значення на value. Повертає this.
+ 
+ // Об'єкт класу може проводити послідовні операції у вигляді ланцюжка
+ 
+ // Приклад використання:
+ // const calc = new Calculator();
+
+
+// const result = calc
+//   .number(10)   // Встановлюємо початкове значення 10
+//   .add(5)       // Додаємо 5 (10 + 5 = 15)
+//   .subtract(3)  // Віднімаємо 3 (15 - 3 = 12)
+//   .multiply(4)  // Множимо на 4 (12 * 4 = 48)
+//   .divide(2)    // Ділимо на 2 (48 / 2 = 24)
+//   .getResult(); // Отримуємо результат: 24
+
+
+//   console.log(result); // 24
+
+// class Calculator {
+//     constructor(){
+      
+//     }
+//     number(value) {
+//         return this;
+//     }
+//     getResult() {
+        
+//     }
+//     add() {
+        
+//     }
+//     subtract() {
+         
+//      }
+//     divide() {
+    
+//     }
+//     multiply() {
+        
+//     }
+// }
+
+
+// task13
+  //  Наслідування у класах!
+  // Cтворіть клас `Person`, який містить наступні властивості:
+  //  - `name` - ім'я людини;
+  //  - `age`- вік людини;
+  //  - `gender` - стать людини;
+  //  - `email`- електронна пошта людини.
+  
+  // Крім того, клас `Person` має мати метод `getDetails()`,
+  // який повертає об'єкт з ім'ям, віком, статтю 
+  //та електронною поштою людини.
+  
+  // 
+  // Потім Створіть клас `Employee`, який розширює клас `Person` і містить наступні властивості:
+  //  - salary - зарплата співробітника;
+  //  - department - відділ, в якому працює співробітник.
+  // Крім того, клас `Employee` має мати метод `getEmployeeDetails()`, який повертає об'єкт з зарплатою співробітника та відділом, в якому він працює.
+class Person {
+    name;
+    age;
+    gender;
+    email;
+    constructor(name, age, gender, email) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.email = email;
+    }
+    getDetails() {
+        return {
+            name: this.name,
+            age: this.age,
+            gender: this.gender,
+            email: this.email
+        }
+    }
+   
+}
+class Employee extends Person {
+    constructor(name, age, gender, email, salary, department) {
+        super(name, age, gender, email);
+        this.salary = salary;
+        this.department = department;
+    }
+    getEmployeeDetails() {
+        return {
+            salary: this.salary,
+            department: this.department
+        }
+    }
+    }
+
+const person1 = new Person('Alex', '23', 'm', 'mail@.com');
+console.log(person1.getDetails());
+const employee1 = new Employee('Alex', '23', 'm', 'mail@.com', '2000', 'qa');
+
+console.log(employee1.getEmployeeDetails());
+// // Створіть клас BankAccount, який має конструктор для зберігання балансу.
+// Додайте методи:
+// withdraw - метод для зняття готівки. Отримує в якості аргументу
+// суму готівки котру потрібно зняти. Готівку можна зняти за умови,
+// що баланс більше або дорівнює сумі, котру хоче зняти користувач.
+// Виводить повідомлення: `Знято amount грн. Залишок на рахунку: balance грн.`
+// або "Недостатньо коштів на рахунку."
+// deposit - метод для пововнення балансу. Отримує в якості аргументу
+// суму котру користувач хоче покласти на рахунок.
+// Виводить повідомлення: `Поповнено amount грн. Залишок на рахунку: balance грн.`
+// checkBalance - метод для перевірки балансу. 
+// Виводить повідомлення `Залишок на рахунку: balance грн.`
